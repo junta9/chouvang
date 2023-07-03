8822d4ce76d96767d20588565cfa97ee49063a11
Chou VANG <lesolo91@hotmail.com>
2023-07-03 13:42:24 +0200

    maj portrait

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 window.onload = function () {
-    setTimeout(function () {
-      document.querySelector("main").style.display = "block";
-    }, 5000);
-  
-  };
+  setTimeout(function () {
+    document.querySelector("main").style.display = "block";
+  }, 5000);
+};
+
+const btnHtml = document.querySelectorAll(".button-html");
+
+btnHtml.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btnHtml.forEach((otherBtn) => {
+      if (otherBtn !== btn) {
+        otherBtn.classList.remove("active");
+      }
+    });
+    btn.classList.add("active");
+  });
+});
